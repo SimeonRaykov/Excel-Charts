f394ff3a35a7b89d3e81b7882f10539a84a825fe
Simeon Raykov <makenr3@gmail.com>
2020-02-13 16:29:51 +0200

    Add files via upload
    Added new menu - Graphs (STP and hourly). Added default criteria values for most menu pages (the queried data is since last week and all erp types are selected by default).

---
+++ public/js/graphs/stp-graph-readings.js
@@ -0,0 +1,222 @@
+;
+$(document).ready(function () {
+    visualizeAllInputFromGetParams();
+    getDataListing();
+    listGraphReadingsFiltered();
+});
+
+function getGraphReadings(data) {
+    let i = 0;
+    for (let el in data) {
+        const date = data[el]['date'];
+        const fullDate = new Date(date);
+        const fixedDate = `${fullDate.getFullYear()}-${fullDate.getMonth()+1}-${fullDate.getDate()}`;
+        const erpType = data[el]['erp_type'] == 1 ? 'ИВН' : data[el]['erp_type'] == 2 ? 'ЧЕЗ' : 'ЕнергоПРО';
+        const amount = data[el]['amount'];
+        let currRow = $('<tr>').attr('role', 'row');
+        if (i % 2 == 1) {
+            currRow.addClass('even');
+        } else {
+            currRow.addClass('odd');
+        }
+        i += 1;
+        currRow
+            .append(`<td><a href=/users/clients/stp-hour-reading/daily/s?id=${data[el]['id']}&date=${fixedDate}>${data[el]['id']}</td>`)
+            .append($(`<td><a href=/users/clients/info/${data[el]['cId']}>${data[el]['ident_code']}</a></td>`))
+            .append($('<td>' + data[el]['client_name'] + '</td>'))
+            .append($('<td>' + fixedDate + '</td>'))
+            .append($('<td>' + erpType + '</td>'))
+            .append($('<td>' + amount + '</td>'))
+            .append($('</tr>'));
+        currRow.appendTo($('#tBody'));
+    }
+    // Order DESC
+    dataTable = $('#stp-graph-readings-table').DataTable({
+        "order": [
+            [0, "asc"]
+        ],
+        retrieve: true
+    });
+    $('#tBody').addClass('text-center');
+    $('#list-readings > thead').addClass('text-center');
+}
+
+function getDataListing() {
+    $.ajax({
+        url: '/api/data-listings/graphs-stp-readings',
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            convertDataToSet(data);
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+
+function convertDataToSet(data) {
+    let clientNames = [];
+    let clientIDs = [];
+    for (let num in data) {
+        clientNames.push(data[num].client_name);
+        clientIDs.push(data[num].ident_code);
+    }
+    let uniqueClientNames = removeDuplicatesFromArr(clientNames);
+    visualizeDataListings([uniqueClientNames, clientIDs]);
+}
+
+function visualizeDataListings(arr) {
+    let clientNames = arr[0];
+    let clientIds = arr[1]
+
+    for (let name of clientNames) {
+        $('#stp-hour-readings-clients').append(`<option value="${name}"></option>`);
+    }
+
+    for (let ID of clientIds) {
+        $('#idList').append(`<option value="${ID}"></option>`);
+    }
+}
+
+$('#searchBtn').on('click', (event) => {
+    event.preventDefault();
+    dataTable.clear().destroy();
+    dataTable;
+    let fromDate = $('#fromDate').val();
+    let toDate = $('#toDate').val();
+    let nameOfClient = $('#name').val();
+    let clientID = $('#clientID').val();
+    let erp = $('#erp').val();
+    listGraphReadingsFiltered([fromDate, toDate, nameOfClient, clientID, erp]);
+});
+
+function listGraphReadingsFiltered(arr) {
+    if (!arr) {
+        var name = findGetParameter('name');
+        var fromDate = findGetParameter('fromDate');
+        var toDate = findGetParameter('toDate');
+        var clientID = findGetParameter('clientID');
+        var erp = []
+        if (window.location.href.includes('energoPRO')) {
+            erp.push(3);
+        }
+        if (window.location.href.includes('cez')) {
+            erp.push(2);
+        }
+        if (window.location.href.includes('evn')) {
+            erp.push(1);
+        }
+
+    } else {
+        var [
+            fromDate,
+            toDate,
+            name,
+            clientID,
+            erp
+        ] = arr;
+    }
+    notification('Loading...', 'loading');
+    $.ajax({
+        url: `/api/filter/list-stp-graph-readings`,
+        method: 'POST',
+        data: {
+            fromDate,
+            toDate,
+            name,
+            ident_code: clientID,
+            erp
+        },
+        dataType: 'json',
+        success: function (data) {
+            getGraphReadings(data);
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+    toastr.clear();
+};
+
+function findGetParameter(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function getThisAndLastMonthDates() {
+    let today = new Date();
+    let thisMonthDate = `${today.getFullYear()}-${Number(today.getMonth())+1}-${today.getDay()}`;
+    let lastMonthDate = `${today.getFullYear()}-${Number(today.getMonth())}-${today.getDay()}`;
+    if (Number(today.getMonth()) - 1 === -1) {
+        lastMonthDate = `${Number(today.getFullYear())-1}-${Number(today.getMonth())+12}-${today.getDay()}`;
+    }
+    return [thisMonthDate, lastMonthDate];
+}
+
+function removeDuplicatesFromArr(arr) {
+    let uniqueNames = [];
+    $.each(arr, function (i, el) {
+        if ($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+    });
+    return uniqueNames;
+}
+
+function notification(msg, type) {
+    toastr.clear();
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    if (type == 'error') {
+        toastr.error(msg);
+    } else if (type == 'success') {
+        toastr.success(msg);
+    } else if (type == 'loading') {
+        toastr.info(msg);
+    }
+};
+
+function visualizeAllInputFromGetParams() {
+    visualizeCheckboxesFromHistoryLocation();
+    visualizeInputFromGetParams();
+}
+
+function visualizeInputFromGetParams() {
+    findGetParameter('fromDate') === null ? '' : $('#fromDate').val(findGetParameter('fromDate'));
+    findGetParameter('toDate') === null ? '' : $('#toDate').val(findGetParameter('toDate'));
+    findGetParameter('name') === null ? '' : $('#nameOfClient').val(findGetParameter('name'));
+    findGetParameter('clientID') === null ? '' : $('#clientID').val(findGetParameter('clientID'));
+    findGetParameter('erp') === null ? '' : $('#erp').val(findGetParameter('erp'));
+}
+
+function visualizeCheckboxesFromHistoryLocation() {
+    const location = window.location.href;
+    if (!location.includes('energoPRO')) {
+        $('#energoPRO').prop('checked', false);
+    }
+    if (!location.includes('cez')) {
+        $('#cez').prop('checked', false);
+    }
+    if (!location.includes('evn')) {
+        $('#evn').prop('checked', false);
+    }
+}
--- public/js/menu/admin-lte-menu.js
+++ public/js/menu/admin-lte-menu.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    setDefaultDateForLinks();
+
     // Remove active for all items.
 
     $('.page-sidebar-menu li').removeClass('active');
@@ -25,6 +28,31 @@ $(document).ready(function () {
            }
        } */
 });
+
+function setDefaultDateForLinks() {
+    const lastWeekDate = getLastWeek();
+    const today = new Date();
+    let formattedToday = `${today.getFullYear()}-${today.getMonth()+1<10?`0${today.getMonth()+1}`:today.getMonth()+1}-${today.getDate()<10?`0${today.getDate()}`:today.getDate()}`;
+    let formattedLastWeek = `${lastWeekDate.getFullYear()}-${lastWeekDate.getMonth()+1<10?`0${lastWeekDate.getMonth()+1}`:lastWeekDate.getMonth()+1}-${lastWeekDate.getDate()<10?`0${lastWeekDate.getDate()}`:lastWeekDate.getDate()}`;
+
+    //  Change readings href
+    $('#readings-stp-href').attr('href', `/users/list/readings?fromDate=${formattedLastWeek}&toDate=${formattedToday}&name=&clientID=&erp_type=evn&erp_type=cez&erp_type=energoPRO`);
+    $('#readings-href').attr('href', `/users/list/hour-readings?fromDate=${formattedLastWeek}&toDate=${formattedToday}&name=&clientID=&erp_type=evn&erp_type=cez&erp_type=energoPRO`);
+
+    //  Change graphs href
+    $('#stp-graph-readings-href').attr('href', `/users/list/stp-graph-readings/?fromDate=${formattedLastWeek}&toDate=${formattedToday}&name=&clientID=&erp_type=evn&erp_type=cez&erp_type=energoPRO`);
+    $('#graph-readings-href').attr('href', `/users/list/graph-readings/?fromDate=${formattedLastWeek}&toDate=${formattedToday}&name=&clientID=&erp_type=evn&erp_type=cez&erp_type=energoPRO`);
+
+    //  Invoicing href
+    $('#invoicing-readings-href').attr('href', `/users/invoicing?fromDate=${formattedLastWeek}&toDate=${formattedToday}&erp_type=evn&erp_type=cez&erp_type=energoPRO&clientNames=&clientID=`);
+}
+
+function getLastWeek() {
+    var today = new Date();
+    var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+    return lastWeek;
+}
+
 
 function nth_occurrence(string, char, nth) {
     var first_index = string.indexOf(char);
